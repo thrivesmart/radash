59be57e83cc313ff338a66250dd4c84ccf168162
Matthew Moore <m3moore@gmail.com>
2015-04-29 16:46:34 -0700

    CRUD navigation for campaigns

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,10 +1,12 @@
 class CampaignsController < ApplicationController
+  before_action :set_org
+  before_filter :enforce_org_administrator
   before_action :set_campaign, only: [:show, :edit, :update, :destroy]
 
   # GET /campaigns
   # GET /campaigns.json
   def index
-    @campaigns = Campaign.all
+    @campaigns = @org.campaigns
   end
 
   # GET /campaigns/1
@@ -14,7 +16,7 @@ class CampaignsController < ApplicationController
 
   # GET /campaigns/new
   def new
-    @campaign = Campaign.new
+    @campaign = @org.campaigns.build
   end
 
   # GET /campaigns/1/edit
@@ -24,12 +26,12 @@ class CampaignsController < ApplicationController
   # POST /campaigns
   # POST /campaigns.json
   def create
-    @campaign = Campaign.new(campaign_params)
+    @campaign = @org.campaigns.build(campaign_params)
 
     respond_to do |format|
       if @campaign.save
-        format.html { redirect_to @campaign, notice: 'Campaign was successfully created.' }
-        format.json { render :show, status: :created, location: @campaign }
+        format.html { redirect_to [@campaign.org, @campaign], notice: 'Campaign was successfully created.' }
+        format.json { render :show, status: :created, location: [@campaign.org, @campaign] }
       else
         format.html { render :new }
         format.json { render json: @campaign.errors, status: :unprocessable_entity }
@@ -42,8 +44,8 @@ class CampaignsController < ApplicationController
   def update
     respond_to do |format|
       if @campaign.update(campaign_params)
-        format.html { redirect_to @campaign, notice: 'Campaign was successfully updated.' }
-        format.json { render :show, status: :ok, location: @campaign }
+        format.html { redirect_to [@campaign.org, @campaign], notice: 'Campaign was successfully updated.' }
+        format.json { render :show, status: :ok, location: [@campaign.org, @campaign] }
       else
         format.html { render :edit }
         format.json { render json: @campaign.errors, status: :unprocessable_entity }
@@ -56,15 +58,22 @@ class CampaignsController < ApplicationController
   def destroy
     @campaign.destroy
     respond_to do |format|
-      format.html { redirect_to campaigns_url, notice: 'Campaign was successfully destroyed.' }
+      format.html { redirect_to org_campaigns_url(@campaign.org), notice: 'Campaign was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_org
+    @org = Org.find_by_permalink(params[:org_id])
+    raise ActiveRecord::RecordNotFound unless @org
+  end
+  
     # Use callbacks to share common setup or constraints between actions.
     def set_campaign
-      @campaign = Campaign.find(params[:id])
+      @campaign = @org.campaigns.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,7 +1,13 @@
 class Campaign < ActiveRecord::Base
+  GOAL_TYPES = { click_roi: "% ROI from Click-Thru Conversions", view_and_click_roi: "% ROI from Click-Thru and View-Thru Conversions", view_reach: "View Reach: Total number of different people viewed.", engagement_reach: "Engagement Reach: Total number of different people commented and voted." }
+  VALID_GOAL_TYPES = GOAL_TYPES.keys.map(&:to_s)
+  
   belongs_to :org
   has_and_belongs_to_many :ads
   
   validates :org, presence: true
   validates :total_budget_in_cents, presence: true
+  validates :goal_type, inclusion: { in: VALID_GOAL_TYPES, message: "%{value} bid type is not valid.", allow_nil: true }
+  
+  serialize :flights, JSON
 end
